2de9d5d7cf095b39744ea9e6f25eb6a16d2ff853
김현진 <66352578+hyunjin998@users.noreply.github.com>
2025-05-10 21:10:36 +0900

    Title: 특정 원소들의 합, Time: 179ms, Memory: 15MB, Status: Passed - Codetree

--- 250510/특정 원소들의 합/sum-of-specific-elements.js
+++ 250510/특정 원소들의 합/sum-of-specific-elements.js
@@ -1,11 +1,2 @@
-const fs = require('fs');
-const arr = fs.readFileSync(0).toString().trim().split('\n')
-.map(line => line.split(' ').map(Number));
-
-let sum = 0;
-for(let i=0; i<4; i++){
-    for(let j=0; j<=i; j++){
-        sum += arr[i][j];
-    }
-}
-console.log(sum);
+console.log(require('fs').readFileSync(0).toString().trim().split('\n').map(line => line.split(' ').map(Number))
+.reduce((s, r, i) => s + r.slice(0, i+1).reduce((a, b) => a + b, 0), 0));
