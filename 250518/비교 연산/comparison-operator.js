07c37fca72b025c59b37e60b9e094f869f649394
김현진 <66352578+hyunjin998@users.noreply.github.com>
2025-05-18 00:52:02 +0900

    Title: 비교 연산, Time: 198ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250518/비교 연산/comparison-operator.js
@@ -0,0 +1,7 @@
+const [A, B] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number);
+console.log(A >= B ? '1': '0');
+console.log(A > B ? '1' : '0');
+console.log(B >= A ? '1' : '0');
+console.log(B > A ? '1' : '0');
+console.log(A === B ? '1' : '0');
+console.log(A != B ? '1' : '0');
