0d3b13ec9666cb7bb47cb322bdc378163fc1c13a
김현진 <66352578+hyunjin998@users.noreply.github.com>
2025-05-17 15:51:00 +0900

    Title: 간단한 사칙연산, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250517/간단한 사칙연산/simple-arithmetic-operation.js
+++ 250517/간단한 사칙연산/simple-arithmetic-operation.js
@@ -1,2 +1,2 @@
 const [A, B] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number);
-console.log((A+B) + '\n' + (A-B) + '\n' + (A/B).toFixed(0) + '\n' + (A%B));
+console.log((A+B) + '\n' + (A-B) + '\n' + Math.floor(A/B) + '\n' + (A%B));
