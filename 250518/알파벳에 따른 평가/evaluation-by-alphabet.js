2c548d08692e1525fc6d811a417e8069172bd259
김현진 <66352578+hyunjin998@users.noreply.github.com>
2025-05-18 00:32:38 +0900

    Title: 알파벳에 따른 평가, Time: 187ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250518/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -0,0 +1,2 @@
+const char = require('fs').readFileSync(0).toString().trim();
+console.log(char === 'S' ? 'Superior' : char === 'A' ? 'Excellent' : char === 'B' ? 'Good' : char === 'C' ? 'Usually' : char === 'D' ? 'Effort' : 'Failure');
